f1b5527d397ecbea7e323ef1365ba77b8c3eaa5e
Abdullah Azhar Khan <109475658+abdullahazharkhan@users.noreply.github.com>
2023-01-06 22:35:36 +0500

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Box
+      as="section"
+      color="gray.50"
+      bg="purple.600"
+      pt={["60px", "60px", "90px"]}
+      pb="198px"
+      px="8"
+      textAlign={["left", "left", "center"]}
+    >
+      <Heading fontWeight="extrabold" fontSize={["3xl", "3xl", "5xl"]}>
+        Simple pricing for your business
+      </Heading>
+      <Text fontWeight="medium" fontSize={["lg", "lg", "2xl"]} pt="4">
+        Plans that are carefully crafted to suit your business.
+      </Text>
+    </Box>
+  );
+}
